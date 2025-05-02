6a90e1f15262c97fe7feaf2080fcdc4c2c2510e2
demouth <yuya@demouth.net>
2025-05-03 08:34:02 +0900

    test: add go-sql-driver/mysql

--- tests/go.mod
+++ tests/go.mod
@@ -4,4 +4,9 @@ go 1.24.1
 
 replace github.com/demouth/mysqldriver => ../
 
-require github.com/demouth/mysqldriver v0.0.0-00010101000000-000000000000
+require (
+	github.com/demouth/mysqldriver v0.0.0-00010101000000-000000000000
+	github.com/go-sql-driver/mysql v1.9.2
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- tests/main.go
+++ tests/main.go
@@ -2,40 +2,43 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/demouth/mysqldriver"
-	// _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// using driver: mysqldriver
+
 	db2, err := sql.Open("mysqldriver", "user:password@tcp(localhost:9910)/test?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db2.Close()
-	println("Ping", db2.Ping())
+	fmt.Println("Ping", db2.Ping())
+	// r, e := db2.Exec("SHOW DATABASES")
+	// fmt.Printf("SHOW DATABASES: %#v, %v\n", r, e)
 
-	/*
-		db, err := sql.Open("mysql", "user:password@tcp(localhost:9910)/test?charset=utf8&parseTime=True&loc=Local")
-		if err != nil {
+	// using driver: mysql
+
+	db, err := sql.Open("mysql", "user:password@tcp(localhost:9911)/test?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	fmt.Println("Ping", db.Ping())
+	rows, err := db.Query("SHOW DATABASES")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var database string
+		if err := rows.Scan(&database); err != nil {
 			panic(err.Error())
 		}
-		defer db.Close()
-
-		println("Ping", db.Ping())
-		r, e := db.Exec("SHOW DATABASES")
-		println(r, e)
-		rows, err := db.Query("SHOW DATABASES")
-		if err != nil {
-			panic(err.Error())
-		}
-		defer rows.Close()
-		for rows.Next() {
-			var database string
-			if err := rows.Scan(&database); err != nil {
-				panic(err.Error())
-			}
-			fmt.Println(database)
-		}
-	*/
+		fmt.Println(database)
+	}
 }
